8f5c9814b2af13cf9bbcc203490daaf4ee580dd5
Lakshman <Lakshman1055@yahoo.com>
2021-06-03 01:31:21 -0500

    added db context method

---
+++ DataAccess/PlutoContext.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirstDemo.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.DataAccess
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext():base("name=DefaultConnection")
+        {
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Course> Courses{ get; set; }
+        public DbSet<Tag> Tags{ get; set; }
+
+    }
+}
